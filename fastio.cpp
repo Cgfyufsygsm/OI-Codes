abab30503160ad553525f7eec397f252ec8495a8
YangTY <375836877@qq.com>
2021-07-21 11:22:11 +0800

    new IO lib

---
+++ fastio.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <cctype>
+#define il inline
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define DEC(i, a, b) for (int i = a; i >= b; --i)
+
+namespace fastIO {
+    const int maxc = 1e7 + 5;
+    char ibuf[maxc], *__p1 = ibuf, *__p2 = ibuf;
+    typedef int IO_int;
+    il char getchar() {return __p1 == __p2 && (__p2 = (__p1 = ibuf) + fread(ibuf, 1, maxc, stdin), __p1 == __p2) ? EOF : *__p1++;}
+    IO_int read() {
+        IO_int s = 0, x = 0;
+        char c = getchar();
+        while (!isdigit(c))
+            x |= (c == '-'), c = getchar();
+        while (isdigit(c))
+            s = s * 10 + c - '0', c = getchar();
+        return x ? -s : s;
+    }
+    char obuf[maxc], *__pO = obuf;
+    il void putchar(char c) {*__pO++ = c;}
+    void print(IO_int x) {
+        if (x < 0) putchar('-'), print(-x);
+        else {
+            if (x > 9) print(x / 10);
+            putchar(x % 10 + '0');
+        }
+        return;
+    }
+    void output() {fwrite(obuf, __pO - obuf, 1, stdout);}
+}
+
+using namespace fastIO;
+
+int main() {}
