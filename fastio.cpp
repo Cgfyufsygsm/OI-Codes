441a3776062993514d8ae927e87b579a5c1f1f34
YangTY <54734412+Cgfyufsygsm@users.noreply.github.com>
2021-07-21 21:14:59 +0800

    update io lib

--- fastio.cpp
+++ fastio.cpp
@@ -5,22 +5,21 @@
 #define DEC(i, a, b) for (int i = a; i >= b; --i)
 
 namespace fastIO {
-    const int maxc = 1e7 + 5;
+    const int maxc = 1 << 21;
     char ibuf[maxc], *__p1 = ibuf, *__p2 = ibuf;
-    typedef int IO_int;
     il char getchar() {return __p1 == __p2 && (__p2 = (__p1 = ibuf) + fread(ibuf, 1, maxc, stdin), __p1 == __p2) ? EOF : *__p1++;}
-    IO_int read() {
-        IO_int s = 0, x = 0;
+    template<typename T> void read(T &n) {
+        int x = 0; n = 0;
         char c = getchar();
         while (!isdigit(c))
             x |= (c == '-'), c = getchar();
         while (isdigit(c))
-            s = s * 10 + c - '0', c = getchar();
-        return x ? -s : s;
+            n = n * 10 + c - '0', c = getchar();
+        n = x ? -n : n;
     }
     char obuf[maxc], *__pO = obuf;
     il void putchar(char c) {*__pO++ = c;}
-    void print(IO_int x) {
+    template<typename T> void print(T x) {
         if (x < 0) putchar('-'), print(-x);
         else {
             if (x > 9) print(x / 10);
@@ -33,4 +32,6 @@ namespace fastIO {
 
 using namespace fastIO;
 
-int main() {}
+int main() {
+    return output(), 0;
+}
