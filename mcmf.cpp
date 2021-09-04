4bb3e48de2fcef492a33bc7369b0b96a9ee0ac12
yangty <375836877@qq.com>
2021-09-05 00:36:19 +0800

    Create mcmf.cpp

---
+++ mcmf.cpp
@@ -0,0 +1,126 @@
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <queue>
+#define il inline
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define DEC(i, a, b) for (int i = (a); i >= (b); --i)
+#define GO(u, x) for (int i = x[u], v = e[i].to; i; i = e[i].nxt, v = e[i].to)
+
+namespace fastIO {
+const int maxc = 1 << 23;
+char ibuf[maxc], *__p1 = ibuf, *__p2 = ibuf;
+il char getchar() {return __p1 == __p2 && (__p2 = (__p1 = ibuf) + fread(ibuf, 1, maxc, stdin), __p1 == __p2) ? EOF : *__p1++;}
+template<typename T> void read(T &n) {
+    int x = 0; n = 0;
+    char c = getchar();
+    while (!isdigit(c))
+        x |= (c == '-'), c = getchar();
+    while (isdigit(c))
+        n = n * 10 + c - '0', c = getchar();
+    n = x ? -n : n;
+}
+void read(char *s) {
+    int p = 0;
+    char c = getchar();
+    while (!isdigit(c) && !isalpha(c)) c = getchar();
+    while (isalpha(c) || isdigit(c)) s[p++] = c, c = getchar();
+    return;
+}
+char obuf[maxc], *__pO = obuf;
+il void putchar(char c) {*__pO++ = c;}
+template<typename T> void print(T x) {
+    if (x < 0) putchar('-'), print(-x);
+    else {
+        if (x > 9) print(x / 10);
+        putchar(x % 10 + '0');
+    }
+    return;
+}
+void output() {fwrite(obuf, __pO - obuf, 1, stdout);}
+} // namespace fastIO
+
+using namespace fastIO;
+
+template<typename T> il T min(const T &a, const T &b) {return a < b ? a : b;}
+template<typename T> il T max(const T &a, const T &b) {return a > b ? a : b;}
+
+typedef long long ll;
+const ll INF = 1e18;
+const int maxn = 5e3 + 5, maxm = 5e4 + 5;
+
+struct edge {
+    int to, nxt;
+    ll w, c;
+} e[maxm << 1];
+
+int head[maxn], cnt = 1, s, t;
+
+il void add(int u, int v, ll w, ll c) {
+    e[++cnt].to = v, e[cnt].w = w, e[cnt].c = c;
+    e[cnt].nxt = head[u], head[u] = cnt;
+    e[++cnt].to = u, e[cnt].w = 0, e[cnt].c = -c;
+    e[cnt].nxt = head[v], head[v] = cnt;
+    return;
+}
+
+namespace MCMF {
+ll dis[maxn], flow[maxn], maxflow, mincost;
+int pre[maxn], inq[maxn];
+
+bool SPFA() {
+    memset(inq, 0, sizeof inq);
+    memset(dis, 0x3f, sizeof dis);
+    std::queue<int> q;
+    q.push(s);
+    inq[s] = 1, dis[s] = 0;
+    flow[s] = INF;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        inq[u] = 0;
+        GO(u, head) {
+            if (e[i].w > 0 && dis[u] + e[i].c < dis[v]) {
+                dis[v] = dis[u] + e[i].c;
+                flow[v] = min(flow[u], e[i].w);
+                pre[v] = i;
+                if (!inq[v]) {
+                    inq[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
+    }
+    return dis[t] < INF;
+}
+
+void update() {
+    int now = t;
+    while (now != s) {
+        int i = pre[now];
+        e[i].w -= flow[t], e[i ^ 1].w += flow[t];
+        now = e[i ^ 1].to;
+    }
+    maxflow += flow[t];
+    mincost += flow[t] * dis[t];
+    return;
+}
+
+void MCMF() {
+    while (SPFA()) update();
+    return;
+}
+} // namespace MCMF
+
+int n, m;
+
+int main() {
+    read(n), read(m), read(s), read(t);
+    FOR(i, 1, m) {
+        int u, v, w, c;
+        read(u), read(v), read(w), read(c);
+        add(u, v, w, c);
+    }
+    MCMF::MCMF();
+    print(MCMF::maxflow), putchar(' '), print(MCMF::mincost);
+    return output(), 0;
+}
