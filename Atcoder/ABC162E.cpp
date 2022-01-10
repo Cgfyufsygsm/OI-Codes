40ad8a204d75900bcc8b794cd2292acfd6f24aa3
yangty <375836877@qq.com>
2022-01-10 22:06:53 +0800

    update recent code

---
+++ Atcoder/ABC162E.cpp
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+#define il inline
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define DEC(i, a, b) for (int i = (a); i >= (b); --i)
+
+using namespace std;
+
+namespace YangTY {
+
+const int mod = 1e9 + 7;
+
+struct modint {
+    typedef int INT;
+    static const INT mod = YangTY::mod;
+    INT val;
+    il void check() {
+        val >= mod ? val %= mod : true;
+        val < 0 ? (val %= mod) += mod : true;
+        return;
+    }
+    modint(INT v = 0) : val(v) {check();}
+
+    il modint &operator=(INT v) {return val = v, *this;}
+    il modint &operator+=(modint rhs) {return val = val + rhs.val >= mod ? val + rhs.val - mod : val + rhs.val, *this;}
+    il modint &operator-=(modint rhs) {return val = val - rhs.val < 0 ? val - rhs.val + mod : val - rhs.val, *this;}
+    il modint &operator*=(modint rhs) {return val = 1ll * val * rhs.val % mod, *this;}
+
+    il friend modint operator+(const modint &lhs, const modint &rhs) {return modint(lhs) += rhs;}
+    il friend modint operator-(const modint &lhs, const modint &rhs) {return modint(lhs) -= rhs;}
+    il friend modint operator*(const modint &lhs, const modint &rhs) {return modint(lhs) *= rhs;}
+    il friend bool operator==(const modint &lhs, const modint &rhs) {return lhs.val == rhs.val;}
+    il friend bool operator!=(const modint &lhs, const modint &rhs) {return lhs.val != rhs.val;}
+    il friend bool operator>(const modint &lhs, const modint &rhs) {return lhs.val > rhs.val;}
+    il friend bool operator<(const modint &lhs, const modint &rhs) {return lhs.val < rhs.val;}
+    il friend bool operator>=(const modint &lhs, const modint &rhs) {return lhs.val >= rhs.val;}
+    il friend bool operator<=(const modint &lhs, const modint &rhs) {return lhs.val <= rhs.val;}
+
+    il modint &operator++() {
+        ++val;
+        if (val == mod) val = 0;
+        return *this;
+    }
+    il modint &operator--() {
+        if (val == 0) val = mod;
+        --val;
+        return *this;
+    }
+    il modint operator++(int) {
+        modint ret = *this;
+        ++*this;
+        return ret;
+    }
+    il modint operator--(int) {
+        modint ret = *this;
+        --*this;
+        return ret;
+    }
+
+    il modint operator+() const {return *this;}
+    il modint operator-() const {return modint() - *this;}
+};
+
+modint qPow(modint base, modint exp) {
+    base.check();
+    modint ret = 1;
+    for (auto p = exp.val; p; p >>= 1, base *= base)
+        if (p & 1) ret *= base;
+    return ret;
+}
+
+namespace fastIO {
+const int maxc = 1 << 23;
+char ibuf[maxc], *__p1 = ibuf, *__p2 = ibuf;
+il char getchar() {return __p1 == __p2 && (__p2 = (__p1 = ibuf) + fread(ibuf, 1, maxc, stdin), __p1 == __p2) ? EOF : *__p1++;}
+template<typename T> void read(T &n) {
+    int x = 0; n = 0;
+    char c = getchar();
+    while (!isdigit(c))
+        x |= (c == '-'), c = getchar();
+    while (isdigit(c))
+        n = n * 10 + c - '0', c = getchar();
+    n = x ? -n : n;
+}
+void read(char *s) {
+    int p = 0;
+    char c = getchar();
+    while (isspace(c)) c = getchar();
+    while (~c && !isspace(c)) s[p++] = c, c = getchar();
+    return;
+}
+template<typename T1, typename... T2> void read(T1 &a, T2&... x) {
+    read(a), read(x...);
+    return;
+}
+char obuf[maxc], *__pO = obuf;
+il void putchar(char c) {*__pO++ = c;}
+template<typename T> void print(T x, char c = '\n') {
+    static char stk[50];
+    int top = 0;
+    if (x < 0) putchar('-'), x = -x;
+    if (x) {
+        for (; x; x /= 10) stk[++top] = x % 10 + '0';
+        while (top) putchar(stk[top--]);
+    } else putchar('0');
+    putchar(c);
+    return;
+}
+void print(char *s, char c = '\n') {
+    for (int i = 0; s[i]; ++i) putchar(s[i]);
+    putchar(c);
+    return;
+}
+void print(const char *s, char c = '\n') {
+    for (int i = 0; s[i]; ++i) putchar(s[i]);
+    putchar(c);
+    return;
+}
+il void print(modint x, char c = '\n') {print(x.val, c);}
+template<typename T1, typename... T2> il void print(T1 a, T2... x) {
+    if (!sizeof...(x)) print(a);
+    else print(a, ' '), print(x...);
+    return;
+}
+void output() {fwrite(obuf, __pO - obuf, 1, stdout);}
+} // namespace fastIO
+
+using namespace fastIO;
+
+template<typename T> il T max(const T &a, const T &b) {return a > b ? a : b;}
+template<typename T> il T min(const T &a, const T &b) {return a < b ? a : b;}
+template<typename T, typename...Args> il T max(const T &a, const Args&... args) {
+    T res = max(args...);
+    return max(a, res);
+}
+template<typename T, typename...Args> il T min(const T &a, const Args&... args) {
+    T res = min(args...);
+    return min(a, res);
+}
+template<typename T> il T chkmax(T &a, const T &b) {return a = max(a, b);}
+template<typename T> il T chkmin(T &a, const T &b) {return a = min(a, b);}
+template<typename T> il T myabs(const T &a) {return a >= 0 ? a : -a;}
+template<typename T> il void myswap(T &a, T &b) {
+    T t = a;
+    a = b, b = t;
+    return;
+}
+
+const int maxn = 1e5 + 5;
+int n, k;
+modint f[maxn];
+
+int main() {
+    read(n, k);
+    modint ans = 0;
+    DEC(x, k, 1) {
+        f[x] = qPow(k / x, n);
+        for (int y = 2 * x; y <= k; y += x) f[x] -= f[y];
+        ans += x * f[x];
+    }
+    print(ans);
+    return output(), 0;
+}
+}// namespace YangTY
+
+int main() {
+    YangTY::main();
+    return 0;
+}
---
+++ Atcoder/ABC234H.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+#define il inline
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define DEC(i, a, b) for (int i = (a); i >= (b); --i)
+
+using namespace std;
+
+namespace YangTY {
+namespace fastIO {
+const int maxc = 1 << 23;
+char ibuf[maxc], *__p1 = ibuf, *__p2 = ibuf;
+il char getchar() {return __p1 == __p2 && (__p2 = (__p1 = ibuf) + fread(ibuf, 1, maxc, stdin), __p1 == __p2) ? EOF : *__p1++;}
+template<typename T> void read(T &n) {
+    int x = 0; n = 0;
+    char c = getchar();
+    while (!isdigit(c))
+        x |= (c == '-'), c = getchar();
+    while (isdigit(c))
+        n = n * 10 + c - '0', c = getchar();
+    n = x ? -n : n;
+}
+void read(char *s) {
+    int p = 0;
+    char c = getchar();
+    while (isspace(c)) c = getchar();
+    while (~c && !isspace(c)) s[p++] = c, c = getchar();
+    return;
+}
+template<typename T1, typename... T2> void read(T1 &a, T2&... x) {
+    read(a), read(x...);
+    return;
+}
+char obuf[maxc], *__pO = obuf;
+il void putchar(char c) {*__pO++ = c;}
+template<typename T> void print(T x, char c = '\n') {
+    static char stk[50];
+    int top = 0;
+    if (x < 0) putchar('-'), x = -x;
+    if (x) {
+        for (; x; x /= 10) stk[++top] = x % 10 + '0';
+        while (top) putchar(stk[top--]);
+    } else putchar('0');
+    putchar(c);
+    return;
+}
+void print(char *s, char c = '\n') {
+    for (int i = 0; s[i]; ++i) putchar(s[i]);
+    putchar(c);
+    return;
+}
+void print(const char *s, char c = '\n') {
+    for (int i = 0; s[i]; ++i) putchar(s[i]);
+    putchar(c);
+    return;
+}
+template<typename T1, typename... T2> il void print(T1 a, T2... x) {
+    if (!sizeof...(x)) print(a);
+    else print(a, ' '), print(x...);
+    return;
+}
+void output() {fwrite(obuf, __pO - obuf, 1, stdout);}
+} // namespace fastIO
+
+using namespace fastIO;
+
+template<typename T> il T max(const T &a, const T &b) {return a > b ? a : b;}
+template<typename T> il T min(const T &a, const T &b) {return a < b ? a : b;}
+template<typename T, typename...Args> il T max(const T &a, const Args&... args) {
+    T res = max(args...);
+    return max(a, res);
+}
+template<typename T, typename...Args> il T min(const T &a, const Args&... args) {
+    T res = min(args...);
+    return min(a, res);
+}
+template<typename T> il T chkmax(T &a, const T &b) {return a = max(a, b);}
+template<typename T> il T chkmin(T &a, const T &b) {return a = min(a, b);}
+template<typename T> il T myabs(const T &a) {return a >= 0 ? a : -a;}
+template<typename T> il void myswap(T &a, T &b) {
+    T t = a;
+    a = b, b = t;
+    return;
+}
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int maxn = 2e5 + 5;
+const ll big = 2000000000;
+int n; ll K;
+pll p[maxn];
+map<ll, vector<int> > mp;
+vector<ll> delta = {-big - 1, -big, -big + 1, -1, 0, 1, big - 1, big, big + 1};
+
+int main() {
+    read(n, K);
+    FOR(i, 1, n) {
+        read(p[i].first, p[i].second);
+        ll id = (p[i].first / K) * big + (p[i].second / K);
+        mp[id].push_back(i);
+    }
+    vector<pii> ans;
+    FOR(i, 1, n) {
+        ll id = (p[i].first / K) * big + (p[i].second / K);
+        for (auto &d : delta) {
+            ll curid = id + d;
+            for (auto &j : mp[curid]) {
+                if (j >= i) continue;
+                ll dis = (p[i].first - p[j].first) * (p[i].first - p[j].first) + (p[i].second - p[j].second) * (p[i].second - p[j].second);
+                if (dis <= K * K) ans.push_back(pii(j, i));
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    print(ans.size());
+    for (auto &p : ans) print(p.first, p.second);
+    return output(), 0;
+}
+
+} // namespace YangTY
+
+int main() {
+    YangTY::main();
+    return 0;
+}
